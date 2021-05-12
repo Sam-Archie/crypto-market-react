9744411d2fc78a208431c207c39d31a25f678590
Paul Dredge <dredgos@googlemail.com>
2021-05-12 15:16:44 +0100

    added style sheets for pattern library components

--- src/PatternLib.js
+++ src/PatternLib.js
@@ -8,7 +8,46 @@ const PatternLib = () => {
                 <h2 class="pattern__title">Title Here</h2>
                 <p class="pattern__text">Description here</p>
                 <div class="pattern__example">
-                    <p>Content Here</p>
+                    <h1 className="header_text">Example Header</h1>
+                </div>
+            </article>
+        </div>
+        <div className="container">
+            <article class="pattern">
+                <h2 class="pattern__title">Title Here</h2>
+                <p class="pattern__text">Description here</p>
+                <div class="pattern__example">
+                    <h1 className="title_text">Example Large Text</h1>
+                </div>
+            </article>
+        </div>
+        <div className="container">
+            <article class="pattern">
+                <h2 class="pattern__title">Title Here</h2>
+                <p class="pattern__text">Description here</p>
+                <div class="pattern__example">
+                    <p className="paragraph_text">Example Medium Text</p>
+                </div>
+            </article>
+        </div>
+        <div className="container">
+            <article class="pattern">
+                <h2 class="pattern__title">Title Here</h2>
+                <p class="pattern__text">Description here</p>
+                <div class="pattern__example">
+                    <p className="paragraph_text paragraph_text--small">Example Small Text</p>
+                </div>
+            </article>
+        </div>
+
+        <div className="container">
+            <article class="pattern">
+                <h2 class="pattern__title">Title Here</h2>
+                <p class="pattern__text">Description here</p>
+                <div class="pattern__example">
+                    <button className="button button__primary">Primary</button>
+                    <button className="button button__secondary">Secondary</button>
+                    <button className="button">Button</button>
                 </div>
             </article>
         </div>
@@ -22,7 +61,7 @@ const PatternLib = () => {
                 </div>
             </article>
         </div>
-</>
+    </>
     );
 };
 
