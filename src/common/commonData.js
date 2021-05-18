58e1e0eb9a7fa4d5a905e35125926cc7053572f5
Sam Archie <samuelarchie1@gmail.com>
2021-05-18 13:13:18 +0100

    Need to refacotr a few functions that may be better off as variables

--- src/common/commonData.js
+++ src/common/commonData.js
@@ -6,7 +6,7 @@ export const openingPrices = () => Object.values(coinPrices()).map((date) => dat
 
 export const today = () => new Date().toISOString().slice(0, 10);
 
-export const timeSeries = () => ("Time Series (Digital Currency Daily)");
+export const timeSeries = () => "Time Series (Digital Currency Daily)";
 
 export const coinInformation = () => data[timeSeries];
 
@@ -19,22 +19,26 @@ export const filteredByDaysOpeningPrice = days => openingPrices().filter((date, 
 export const todaysOpeningPrice = () => openingPrices().filter((date, index) => index === 0);
  
 export const priceComparison = () => {
-    let ninetyPrices = filteredByDaysOpeningPrice(90);
-    console.log(ninetyPrices)
-    let colors = ninetyPrices.map((price, index) => {
-         return +price[index] < +price[index + 1] ? "red" : "green"
+
+       let ninetyPrices = filteredByDaysOpeningPrice(91);
+       console.log(ninetyPrices)
+    const colors = ninetyPrices.map((price, index) =>  {
+        if (index === 0) return "green";
+        console.log(+price)
+        return +ninetyPrices[index - 1] < +price ? "red" : "green"
     })
+    console.log(colors);
+    return colors
     
+    
+};
 
-    console.log(colors)
-    // let colors = [];
 
-    // for(let i = openingPrices.length; i <= openingPrices.length && i > 910; i -= 1)
-    // {
-    //     console.log(openingPrices[i])
+describe("testing price comparison", () => {
+    it("works for ninety days", () => {
+        const result = priceComparison(90);
+        expect(result).toHaveLength(90);
+    })
 
-    //     openingPrices[i] > openingPrices[--i] ? colors.unshift("red") : 
-    //         openingPrices[i] === openingPrices[--i] ? colors.unshift("black") : colors.unshift("green");
-    // }
-    // return colors;
-}
+
+})
