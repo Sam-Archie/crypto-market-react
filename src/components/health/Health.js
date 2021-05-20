4dd7c2290e4e41d50076f1611e64e5e241ecf38b
Paul Dredge <dredgos@googlemail.com>
2021-05-20 13:04:59 +0100

    Added health component for displaying health information

--- src/components/health/Health.js
+++ src/components/health/Health.js
@@ -6,7 +6,8 @@ const Health = () => {
 
     return (
         <>
-        
+            <h3>{coin} ratings</h3>
+
         </>
            
     );
@@ -15,3 +16,16 @@ const Health = () => {
 export default Health;
 
  
+// export const healthData = {
+//     "Crypto Rating (FCAS)": {
+//         "1. symbol": "BTC",
+//         "2. name": "Bitcoin",
+//         "3. fcas rating": "Attractive",
+//         "4. fcas score": "894",
+//         "5. developer score": "848",
+//         "6. market maturity score": "877",
+//         "7. utility score": "956",
+//         "8. last refreshed": "2021-05-19 00:00:00",
+//         "9. timezone": "UTC"
+//     }
+// }
