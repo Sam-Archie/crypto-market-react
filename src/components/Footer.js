73e0982b6b56165655f423a196faa8fa8b93d3ab
Paul Dredge <dredgos@googlemail.com>
2021-06-02 09:47:04 +0100

    Added basic media queries, amended spelling errors and updated hero text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="footer">
-            <p className="paragraph_text paragraph_text--small text-center pt-4 pb-4">&copy; 2021 Sam and Paul Development Studios</p>
+            <p className="paragraph_text paragraph_text--small text-center pt-4 pb-4">&copy; 2021 <a href="https://github.com/Sam-Archie">Sam</a> and <a href="https://github.com/dredgos">Paul</a> Development Studios</p>
         </div>
         
     );
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,10 @@ const Hero = () => {
     return (
     <>
         <section id="hero">
-            <p className="title_text title_text--small title_text--hero">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="title_text title_text--small title_text--hero">Investing in Cryptocurrency is growing in popularity as more and more companies support payments using them. This app provides some key information on some of the largest mainstream currencies so you can make informed decisions with your investments.<br/>
+            Check out the data in the coins section to get up to date market information on Bitcoin, Ethereum, Binance-Coin, Tether and Dogecoin.</p>
+
+            <p className="title_text--small title_text--hero hero_text_alternate">Investing in Cryptocurrency is growing in popularity as more and more companies support payments using them. This app provides some key information on some of the largest mainstream currencies so you can make informed decisions with your investments.</p>
         </section>
         <Image className="hero-image" src={ hero }/>
     </>
